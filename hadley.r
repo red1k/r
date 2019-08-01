7bb83d26c233a65e568541e58dab9d30f1ae34b4
red1k <erdenebayar_sh@yahoo.com>
2019-08-02 00:21:37 +0800

    Hadley seminar codes

---
+++ hadley.r
@@ -0,0 +1,64 @@
+library(tidyverse)
+library(gapminder)
+
+glimpse(gapminder)
+unique(gapminder$year)
+
+gapminder %>%
+    filter(year == 2002) ->
+    gapminder02
+
+glimpse(gapminder02)
+
+range(gapminder$lifeExp)
+range(gapminder$pop)
+range(gapminder$gdpPercap)
+
+gapminder02 %>%
+    ggplot(aes(gdpPercap, lifeExp)) +
+    geom_point(aes(color = continent, size = pop, alpha = 0.5)) +
+    scale_size(
+        limits = c(1800, 1420000000),
+        range = c(1, 40),
+        guide = FALSE
+    ) +
+    labs(
+        x = 'Income',
+        y = 'Life Expectancy (year)'
+    )
+
+
+gap_plot <- function(data) {
+    data %>%
+        arrange(desc(pop)) %>%
+        ggplot(aes(gdpPercap, lifeExp)) +
+        geom_point(aes(color = continent, size = pop)) +
+        scale_y_continuous(
+            limits = c(20, 85)
+        ) +
+        scale_x_continuous(
+            limits = c(200, 60000)
+        ) +
+        labs(
+            x = 'Income',
+            y = 'Life Expectancy (year)'
+        )
+}
+
+gapminder %>%
+    filter(country == 'New Zealand') %>%
+    gap_plot()
+
+gapminder3 <- gapminder %>%
+    group_by(year)
+
+by_year <- gapminder3 %>%
+    group_split(year)
+
+year <- gapminder3 %>% group_keys() %>% pull(year)
+title <- paste0('Year: ', year)
+plots <- map2(by_year, title, ~gap_plot(.x))
+paths <- paste0(year, '.png')
+
+# exporting png files
+walk2(paths, plots, ggsave, width = 9, height = 6)
