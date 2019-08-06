51f352b07c138f6b76cb3f0baf086b00723aeb92
red1k <erdenebayar_sh@yahoo.com>
2019-08-06 16:51:47 +0800

    Model managing, example codes

--- webinar/func.r
+++ webinar/func.r
@@ -47,10 +47,12 @@ lm(lifeExp ~ year1950, data = by_country$data[[2]])
 # map(data, country_model) -> 'data' column deer country_model function-g bodoh
 models <- by_country %>%
     mutate(
-        mod = map(data, country_model)
+      # model = data %>% map(country_model)
+        model = map(data, country_model)
     )
 
 mtcars
+
 # for loops
 # all for loops must started with allocated output
 
@@ -78,7 +80,65 @@ typeof(medians)
 funs <- list(mean, median, sd)
 funs
 
+# each of this functions computed for each elements in dataframe
 blah <- funs %>%
     map(~ mtcars %>% map_dbl(.x))
 
+blah
 blah[[2]]
+
+models <- models %>%
+    mutate(
+        glance  = model %>% map(broom::glance),
+        rsq     = glance %>% map_dbl("r.squared"),
+        tidy    = model %>% map(broom::tidy),
+        augment = model %>% map(broom::augment)
+    )
+
+models
+models$glance[[1]]
+models$rsq
+models$tidy[[1]]
+models$augment[[1]]
+
+models %>% arrange(desc(rsq))
+models %>% filter(continent == 'Africa')
+
+models %>%
+    ggplot(aes(rsq, reorder(country, rsq))) +
+    geom_point(aes(color = continent))
+    
+models %>%
+    filter(continent == 'Africa') %>%
+    ggplot(aes(rsq, reorder(country, rsq))) +
+    geom_point()
+
+# unnest function will extract nested column 'data' 
+# nest hiigeegui df rsq baganiin utgatai hamt
+models %>% unnest(data)
+models %>% unnest(glance, .drop = TRUE) %>% View()
+models %>% unnest(tidy)
+
+models %>%
+    unnest(tidy) %>%
+    select(continent, country, term, estimate, rsq) %>%
+    spread(term, estimate) %>% 
+    ggplot(aes(`(Intercept)`, year1950)) +
+    geom_point(aes(color = continent, size = rsq)) +
+    geom_smooth(se = FALSE) +
+    xlab("Life Expectancy (1950)") +
+    ylab("Yearly Improvement") +
+    scale_size_area()
+
+models %>%
+    unnest(augment) %>%
+    ggplot(aes(year1950, .resid)) +
+    geom_line(aes(group = country), alpha = 1/3) +
+    geom_smooth(se = FALSE) +
+    geom_hline(yintercept = 0, color = 'white') +
+    facet_wrap(~continent)
+
+# CONCLUSION
+# 1. Store related objects in list-columns
+# 2. Learn FP so you can focus on verbs, not objects (FP <- functional programming)
+# 3. Use broom to convert models to tidy data
