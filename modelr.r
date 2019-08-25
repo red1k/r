7bc9854e9eb9461d42998129867be8b7ba51f0b4
red1k <erdenebayar_sh@yahoo.com>
2019-08-25 21:30:46 +0800

    Predictive model, residuals and its visualization

--- modelr.r
+++ modelr.r
@@ -1,16 +1,13 @@
 library(tidyverse)
 library(modelr)
+library(reshape2)
 
-nlevels(sales$ID)
-table(sales$ID)
-table(sales$Insp) / nrow(sales) * 100
-
+# our data
 glimpse(sim1)
 summary(sim1)
-
 ggplot(sim1) + geom_point(aes(x, y))
 
-models <- tibble(
+random_models <- tibble(
    a1 = runif(250, -20, 40),
    a2 = runif(250, -5, 5)
 )
@@ -18,32 +15,31 @@ models <- tibble(
 ggplot(sim1, aes(x, y)) +
     geom_abline(
         aes(intercept = a1, slope = a2),
-        data = models, alpha = 1/4
+        data = random_models, alpha = 1/4
     ) +
     geom_point()
 
-model1 <- function(a, data) {
+base_model <- function(a, data) {
     a[1] + data$x * a[2]
 }
 
-model1(c(7, 1.5), sim1)
+base_model(c(7, 1.5), sim1)
 
-# calculating error
+# formula that calculates error
 measure_distance <- function(mod, data) {
-    diff <- data$y - model1(mod, data)
+    diff <- data$y - base_model(mod, data)
     sqrt(mean(diff ^ 2))
 }
 
 measure_distance(c(7, 1.5), sim1)
 
 # helper function to compute the distance for all the models
-sim1_dist <-function(a1, a2) {
+sim1_dist <- function(a1, a2) {
     measure_distance(c(a1, a2), sim1)
 }
 
-models <- models %>%
+models <- random_models %>%
     mutate(dist = purrr::map2_dbl(a1, a2, sim1_dist))
-
 
 # overlaying 10 best models on to the data
 ggplot(sim1, aes(x, y)) +
@@ -52,7 +48,6 @@ ggplot(sim1, aes(x, y)) +
         aes(intercept = a1, slope = a2, color = dist),
         data = filter(models, rank(dist) <= 10)
     )
-models
 
 # seeing all the models at once, highlighted best 10 models in red
 ggplot(models, aes(a1, a2)) +
@@ -74,7 +69,6 @@ grid <- expand.grid(
 ) %>%
     mutate(dist = purrr::map2_dbl(a1, a2, sim1_dist))
 
-
 grid %>% 
     ggplot(aes(a1, a2)) +
     geom_point(
@@ -92,12 +86,111 @@ ggplot(sim1, aes(x, y)) +
         data = filter(grid, rank(dist) <= 10)
     )
 
-best <- optim(c(0, 0), measure_distance, data = sim1)
-best
-best$par
+# using optim() function
+sim1_optim <- optim(c(0, 0), measure_distance, data = sim1)
 
 ggplot(sim1, aes(x, y)) +
     geom_point(size = 2, color = 'grey30') +
     geom_abline(
-        aes(intercept = best$par[1], slope = best$par[2])
+        aes(intercept = sim1_optim$par[1], slope = sim1_optim$par[2])
     )
+
+# using linear model lm()
+sim1_lm <- lm(y ~ x, data = sim1)
+
+# sim1_optim$par = sim1_lm$coefficents
+# coef by optim and coef by lm is exactly the same!
+
+
+# EXERCISES
+
+sim1a <- tibble(
+    x = rep(1:10, each = 3),
+    y = x * 1.5 + 3 +rt(length(x), df = 2)
+)
+
+sim1a %>%
+    ggplot(aes(x, y)) +
+    geom_point() +
+    geom_smooth(method = 'lm')
+    # geom_text(aes(1, 6.374), label = 'blah')
+
+
+sim1a_lm    <- lm(y ~ x, data = sim1a)
+sim1a_optim <- optim(c(0, 0), measure_distance, data = sim1a)
+
+sim1a_optim$par
+sim1a_lm$coefficients
+
+
+# Prediction
+
+# data_grid <- for each subsequent argument it finds the unique variables
+#              and then generates all combinations
+# arguments <- dataframe, subsequent argument (column name)
+grid <- data_grid(sim1, x)
+
+# it adds prediction from the model to a new column in the dataframe
+# arguments <- dataframe, model
+grid <- grid %>% add_predictions(sim1_lm)
+
+# plotting prediction
+ggplot(sim1, aes(x = x)) +
+    geom_point(aes(y = y)) +
+    geom_line(
+        aes(y = pred),
+        data = grid,
+        color = 'red',
+        size = 1
+    )
+
+# Residual <- distance between the observed values and predicted values
+sim1 <- sim1 %>%
+    add_residuals(sim1_lm)
+
+# Visualization
+ggplot(sim1, aes(resid)) +
+    geom_freqpoly(binwidth = 0.5)
+
+ggplot(sim1, aes(x, resid)) +
+    geom_ref_line(h = 0) +
+    geom_point()
+
+
+# Excercise 'Using loess function instead of lm on sim1'
+
+# Model fitting
+sim1_loess <- loess(y ~ x, data = sim1)
+
+# Prediction
+gridWithLoess <- grid %>% add_predictions(sim1_loess)
+
+# Checking differences between loess pred and lm pred
+test <- grid
+test <- test %>%
+    mutate(LoessPred = gridWithLoess$pred,
+           Diff = pred - LoessPred)
+
+# Visualization
+ggplot(sim1, aes(x = x)) +
+    geom_point(aes(y = y)) +
+    geom_line(
+        aes(y = pred),
+        data = gridWithLoess,
+        color = 'red',
+        size = 2
+    ) +
+    geom_smooth(aes(y = y), size = 0.5)
+
+gridWithLoessResidual <- sim1 %>% add_residuals(sim1_loess)
+
+ggplot(gridWithLoessResidual, aes(resid)) +
+    geom_freqpoly(binwidth = 0.5)
+
+ggplot(gridWithLoessResidual, aes(x, resid)) +
+    geom_ref_line(h = 0) +
+    geom_point()
+
+# ggplot tutorial
+# https://www.gl-li.com/2017/08/03/a-uniform-way-to-use-ggplot2/
+# https://ggplot2.tidyverse.org/reference/geom_text.html
