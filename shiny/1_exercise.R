4c9127ddac9bfeec891c3a10bcc3a0fc8753a9b5
red1k <erdenebayar_sh@yahoo.com>
2019-10-20 12:12:15 +0800

    Reactive function added

--- shiny/1_exercise.R
+++ shiny/1_exercise.R
@@ -1,15 +1,19 @@
 library(shiny)
 
-df <- data
 ui <- fluidPage(
     textInput("name", "What is your name?"),
     textOutput("greeting"),
+    
     numericInput("age", label = "How old are you?", value = 18),
     textOutput("ager"),
     plotOutput("hist"),
-    sliderInput(inputId = "x", label = "Multiplier_x", min = 1, max = 50, value = 18),
-    sliderInput(inputId = "y", label = "Multiplier_y", min = 1, max = 50, value = 10),
-    textOutput("result")
+    
+    sliderInput("x", "If x is", min = 1, max = 50, value = 30),
+    sliderInput("y", "and y is", min = 1, max = 50, value = 5),
+    "then, (x * y) is", textOutput("product"),
+    "and, (x * y) + 5 is", textOutput("product_plus5"),
+    "and (x * y) + 10 is", textOutput("product_plus10")
+    
 )
 
 server <- function(input, output, session) {
@@ -31,9 +35,20 @@ server <- function(input, output, session) {
         }
     })
     
-    output$result <- renderText({
-        result = input$x * input$y
-        result
+    multiplied <- reactive({
+        input$x * input$y
+    })
+    
+    output$product <- renderText({
+        multiplied()
+    })
+    
+    output$product_plus5 <- renderText({
+        multiplied() + 5
+    })
+    
+    output$product_plus10 <- renderText({
+        multiplied() + 10
     })
     
 }
